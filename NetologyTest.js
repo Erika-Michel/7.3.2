ea0db0e7d66f933a0f16c2a9feb1cf2beb37922a
Erika-Michel <erikamichel174@gmail.com>
2022-02-07 19:19:44 +0100

    Test workflow - first attempt

--- NetologyTest.js
+++ NetologyTest.js
@@ -1,16 +1,27 @@
+const { test, expect } = require("@playwright/test");
 const { chromium } = require("playwright");
+const { email, password } = require("../user.js");
 
-(async () => {
-  const browser = await chromium.launch({
-    headless: false,
-    slowMo: 5000,
-    devtools: true
-  });
-  const page = await browser.newPage();
-  await page.goto("https://netology.ru");
-  await page.click("text=Каталог курсов");
-  await page.pause();
+test("Should authorize successfully", async ({ page }) => {
+  await page.goto("https://netology.ru/");
+  await page.click("text=Войти");
+  await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+  await page.fill('[placeholder="Email"]', email);
+  await page.fill('[placeholder="Пароль"]', password);
+  await page.click("text=Войти");
 
-  //assertion
-  await browser.close();
-})();
+  await expect(page.locator("text=Мои курсы и профессии")).toBeVisible();
+});
+
+test("Should not authorize", async ({ page }) => {
+  await page.goto("https://netology.ru/");
+  await page.click("text=Войти");
+  await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+  await page.fill('[placeholder="Email"]', "testemail@gmail.com");
+  await page.fill('[placeholder="Пароль"]', password);
+  await page.click("text=Войти");
+
+  await expect(
+    page.locator("text=Вы ввели неправильно логин или пароль")
+  ).toBeVisible();
+});
