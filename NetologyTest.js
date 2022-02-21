a216c820fb82e5bd221f0d160c83847cbc0b6b1e
Erika-Michel <erikamichel174@gmail.com>
2022-02-21 13:45:28 +0100

    Error in firts test - page.fill: value: expected string, got undefined

--- NetologyTest.js
+++ NetologyTest.js
@@ -1,13 +1,13 @@
 const { chromium } = require("playwright");
-const user = require("../playwright/user.js");
+const { email, password } = require("../playwright/user.js");
 const { test, expect } = require("@playwright/test");
-let email = user.email;
-let password = user.password;
+//let email = user.email;
+//let password = user.password;
 
 (async () => {
   const browser = await chromium.launch({
     headless: false,
-    slowMo: 5000,
+    slowMo: 3000,
   });
   const page = await browser.newPage();
   await page.goto("https://netology.ru/?modal=sign_in");
@@ -17,3 +17,21 @@ let password = user.password;
   await expect(page).toHaveURL("https://netology.ru/profile");
   await browser.close();
 })();
+
+(async () => {
+  const browser = await chromium.launch({
+    headless: false,
+    slowMo: 3000,
+  });
+  const page = await browser.newPage();
+  await page.goto("https://netology.ru/?modal=sign_in");
+  await page.fill('[placeholder="Email"]', "test@gmail.com");
+  await page.fill('[placeholder="Пароль"]', "Pass123!!");
+  await page.click("text=Войти");
+  await page.click('button:has-text("Войти")');
+  await expect(
+    page.locator("text=Вы ввели неправильно логин или пароль")
+  ).toBeVisible();
+  await browser.close();
+})();
+
