f14f184f542cf15064571dece5ce30768b1067d6
Erika-Michel <erikamichel174@gmail.com>
2022-02-21 13:13:36 +0100

    Made User as Object

--- NetologyTest.js
+++ NetologyTest.js
@@ -1,5 +1,6 @@
 const { chromium } = require("playwright");
-const user = require("./user.js");
+const user = require("../playwright/user.js");
+const { test, expect } = require("@playwright/test");
 let email = user.email;
 let password = user.password;
 
@@ -7,30 +8,12 @@ let password = user.password;
   const browser = await chromium.launch({
     headless: false,
     slowMo: 5000,
-    devtools: true,
   });
   const page = await browser.newPage();
   await page.goto("https://netology.ru/?modal=sign_in");
   await page.fill('[placeholder="Email"]', email);
   await page.fill('[placeholder="Пароль"]', password);
-  await page.click('button:has-text("Войти")');
-
-  const check = await page.waitForSelector("text=Мои курсы и профессии");
-  await browser.close();
-})();
-
-(async () => {
-  const browser = await chromium.launch({
-    headless: false,
-    slowMo: 5000,
-    devtools: true,
-  });
-  const page = await browser.newPage();
-  await page.goto("https://netology.ru/?modal=sign_in");
-  await page.fill('[placeholder="Email"]', "test@gmail.com");
-  await page.fill('[placeholder="Пароль"]', password);
-  await page.click('button:has-text("Войти")');
-
-  const check = await page.waitForSelector("text=Неверный email");
+  await page.click('text=Войти');
+  await expect(page).toHaveURL("https://netology.ru/profile");
   await browser.close();
 })();
